16c3239c14cfd6eeb919c5dd7bddbee671d00877
Antonio Coelho <antoniocarloscoelho724@gmail.com>
2025-11-16 07:56:11 -0300

    Update Utils.hs
    Refactor registrarLog to accept LogEntry and add readFileIfExists for safe file reading.

--- Utils.hs
+++ Utils.hs
@@ -1,5 +1,41 @@
 module Utils where
 
+import System.IO
+import Control.Exception (try, IOException)
+import Tipos
 
-registrarLog :: String -> IO ()
-registrarLog msg = appendFile "Auditoria.log" (msg ++ "\n")
+--------------------------------------------------------------
+-- REGISTRAR LOG
+--------------------------------------------------------------
+
+registrarLog :: LogEntry -> IO ()
+registrarLog entry =
+    appendFile "Auditoria.log" (show entry ++ "\n")
+
+--------------------------------------------------------------
+-- LER ARQUIVO DE FORMA SEGURA
+--------------------------------------------------------------
+
+readFileIfExists :: FilePath -> IO (Maybe String)
+readFileIfExists path = do
+    mh <- tryOpen path
+    case mh of
+        Nothing -> return Nothing
+        Just h  -> do
+            txt <- hGetContents h
+            length txt `seq` hClose h
+            return (Just txt)
+
+tryOpen :: FilePath -> IO (Maybe Handle)
+tryOpen f = do
+    resultado <- try (openFile f ReadMode) :: IO (Either IOException Handle)
+    case resultado of
+        Left _  -> return Nothing
+        Right h -> return (Just h)
+
+--------------------------------------------------------------
+-- TIMESTAMP SIMPLIFICADO (aceito no trabalho)
+--------------------------------------------------------------
+
+timestampNow :: IO String
+timestampNow = pure "<timestamp>"
