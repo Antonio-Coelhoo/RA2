d24aa66c0a267d74f8d8579762d6895553bb3e69
Antonio Coelho <antoniocarloscoelho724@gmail.com>
2025-11-16 07:55:56 -0300

    Update Tipos.hs
    Added types for inventory management and logging.

--- Tipos.hs
+++ Tipos.hs
@@ -1,6 +1,19 @@
 module Tipos where
 
-
 type Nome = String
 type Quantidade = Int
-type Item = (Nome, Quantidade)
+type Inventario = [(Nome, Quantidade)]
+
+data Acao = Adicionar | Remover | Atualizar | Report
+    deriving (Show, Read, Eq)
+
+data StatusLog = Sucesso | Falha String
+    deriving (Show, Read, Eq)
+
+data LogEntry = LogEntry
+    { timestamp :: String
+    , acao      :: Acao
+    , detalhes  :: String      -- GUARDA SOMENTE O ITEM
+    , status    :: StatusLog
+    }
+    deriving (Show, Read, Eq)
